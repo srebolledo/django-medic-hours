aaf701813cdd9c8d6a7c7943e4436702dc127f01
Stephan Rebolledo <srebolledo@gmail.com>
2013-07-03 08:17:00 -0400

    added changes

--- horasMedicas/settings.py
+++ horasMedicas/settings.py
@@ -132,7 +132,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'bootstrap_toolkit',
-    'adminHours'
+    'adminHours',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
